56b823db06e796cb683b7d4fbb65dc79fe57c4da
Antonio Guiotto <powerhydratoni@gmail.com>
2020-07-20 00:32:40 +0200

    improvements

--- App.js
+++ App.js
@@ -1,121 +1,152 @@
-import React, { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Modal,
-  TextInput,
-  Button,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Modal, TextInput, Button} from 'react-native';
 
 const api_url = 'http://localhost:3000';
 
 const App = () => {
-
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
-  const [isModal, setIsModal] = useState(false);
+  const [isModal, setIsModal] = useState({
+    visible: false,
+    add: false,
+    edit: false,
+    todo: {},
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchTodos(api_url);
   }, []);
 
-  const fetchTodos = async url => {
+  const fetchTodos = async (url) => {
     try {
       const response = await fetch(`${url}/todos`);
       const data = await response.json();
       setTodos(data);
-
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
-  }
+  };
 
-  const handleAddTodo = async data => {
+  const handleAddTodo = async (data) => {
     try {
       const options = {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ data })
-      }
-      await fetch(`${api_url}/todo`, options)
-      fetchTodos(api_url)
+        body: JSON.stringify({data}),
+      };
+      await fetch(`${api_url}/todo`, options);
+      fetchTodos(api_url);
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
-  }
+  };
 
-  // const handleDeleteTodo = async data => {
-  //   try {
-  //     const options = {
-  //       method: 'POST', // fai un soft delete (post request, aggiungi una colonna in DB eg. deleted_on e dagli un flag/valore della data di deletion. Se non hai la deletion date specificata, allora il todo non e' mai stato eliminato )
-  //       // quando fai fetch if (todo == !null) non vengono mostrati nel todolist quando fai fetch
-  //       headers: {'Content-Type': 'application/json'},
-  //       body: JSON.stringify({ data })
-  //     }
-  //     await fetch(`${api_url}/todo`, options)
-  //     fetchTodos(api_url)
-  //   } catch (error) {
-  //     throw new Error(error)
-  //   }
-  // }
+  const handleEditTodo = (newValue) => {
+    console.log('newValue: ', newValue);
+  };
 
-  const renderTodos = todos => {
+  const modalContent = () => {
     return (
       <View>
-        {todos.map(({body}, index) => {
+        {isModal.edit ? (
+          <View>
+            <TextInput
+              placeholder="editing ..."
+              onChangeText={(value) => setInputValue(value)}
+            />
+            <Button
+              title="submit changes"
+              onPress={() => handleEditTodo(inputValue)}
+            />
+          </View>
+        ) : (
+          <View>
+            <TextInput
+              placeholder="write a todo"
+              onChangeText={(value) => setInputValue(value)}
+            />
+            <Button
+              title="add to the list"
+              onPress={() => handleAddTodo(inputValue)}
+            />
+          </View>
+        )}
+        <Button
+          color="#771111"
+          title="close modal"
+          onPress={() =>
+            setIsModal({visible: false, add: false, edit: false, todo: {}})
+          }
+        />
+      </View>
+    );
+  };
+
+  const renderTodos = () => {
+    return (
+      <View>
+        {todos.map(({_id, body}, index) => {
           return (
             <View style={styles.todo} key={index}>
-              <View style={{...styles.todoButton, backgroundColor: 'rgba(0, 255, 0, 0.1)'}}>
-                <Text style={styles.centerText}>✔︎</Text>
+              <View
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  ...styles.todoButton,
+                  backgroundColor: 'rgba(0, 255, 0, 0.1)',
+                }}>
+                <Text style={styles.centerText}>✔</Text>
               </View>
-
-              <View style={styles.todoBody}><Text>{body}</Text></View>
-
-              <View style={{...styles.todoButton, backgroundColor: 'rgba(0, 0, 255, 0.1)'}}>
-                <Text style={styles.centerText}>✎</Text>
+              <View style={styles.todoBody}>
+                <Text
+                  onPress={() =>
+                    setIsModal({
+                      visible: true,
+                      add: false,
+                      edit: true,
+                      todo: {_id, body},
+                    })
+                  }>
+                  {body}
+                </Text>
               </View>
-              <View style={{...styles.todoButton, backgroundColor: 'rgba(255, 0, 0, 0.1)'}}>
+              <View
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  ...styles.todoButton,
+                  backgroundColor: 'rgba(255, 0, 0, 0.1)',
+                }}>
                 <Text style={styles.centerText}>✖︎</Text>
               </View>
             </View>
-          )
+          );
         })}
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <>
-    <View style={styles.scrollView}>
-      <View style={styles.section}>
-        <Button 
-          title="ADD TODO"
-          color="#aabbcc"
-          onPress={() => setIsModal(!isModal)} />
+      <View style={styles.scrollView}>
+        <View style={styles.section}>
+          <Button
+            title="new"
+            color="#aabbcc"
+            onPress={() =>
+              setIsModal({visible: true, add: true, edit: false, todo: {}})
+            }
+          />
           <Modal
             animationType="slide"
             transparent={false}
-            visible={isModal}
-          >
-            <TextInput 
-              placeholder="write a todo" 
-              onChangeText={value => setInputValue(value)} />
-            <Button 
-              title="send"
-              onPress={() => handleAddTodo(inputValue)} />
-            <Button 
-              color="#771111"
-              title="close modal"
-              onPress={() => setIsModal(!isModal)}
-            />
+            visible={isModal.visible}>
+            {modalContent()}
           </Modal>
+        </View>
+        <View style={styles.section}>
+          <Text>Todo list</Text>
+          {todos.length <= 0 ? <Text>empty</Text> : renderTodos()}
+        </View>
       </View>
-      <View style={styles.section}>
-        <Text>Todo list</Text>
-        {todos.length <= 0 ? <Text>empty</Text> : renderTodos(todos)}
-      </View>
-    </View>
     </>
   );
 };
@@ -123,7 +154,7 @@ const App = () => {
 const styles = StyleSheet.create({
   section: {
     marginBottom: 10,
-  },  
+  },
   scrollView: {
     backgroundColor: '#fff',
     padding: 5,
@@ -136,8 +167,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.1)',
   },
   todoBody: {
-    width: '70%',
-    backgroundColor: 'rgba(0,0,0,0.1)'
+    width: '80%',
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    paddingLeft: 5,
+    paddingRight: 5,
   },
   centerText: {
     textAlign: 'center',
